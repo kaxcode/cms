8c5c1943e57751ece6e8d0398af9b44d9a2573cc
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-26 03:28:12 -0400

    Adds params to the url routes

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+Route::get('/contact', function () {
+    return "Hi I am contact";
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name){
+    return "This is post number ". $id . " " . $name;
+});
