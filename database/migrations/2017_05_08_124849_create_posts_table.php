a0269c8158fa96d58fa427e256ee4e08084e6543
Kenia <keniavalladarez@gmail.com>
2017-05-08 08:55:38 -0400

    Created and updated a table migration

--- database/migrations/2017_05_08_124849_create_posts_table.php
+++ database/migrations/2017_05_08_124849_create_posts_table.php
@@ -12,7 +12,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +27,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('posts');
     }
 }
