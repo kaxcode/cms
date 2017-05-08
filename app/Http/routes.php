833e4baf05430bba327b99de297c9ccb754c16da
Kenia <keniavalladarez@gmail.com>
2017-05-08 10:10:22 -0400

    Adds a sql query for selecting and updating

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,11 +16,26 @@ Route::get('/', function () {
 });
 
 
-Route::get('/insert', function (){
+//Route::get('/insert', function (){
+//
+//   DB::insert('insert into posts(title,content) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
+//
+//});
 
-   DB::insert('insert into posts(title,content) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
 
-});
+//  Route::get('/read', function (){
+//    $results = DB::select('select * from posts where id = ?', [1]);
+//
+//    foreach($results as $post) {
+//        return $post->title;
+//    }
+//  });
+
+  Route::get('/update', function (){
+      $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+
+      return $updated;
+  });
 
 Route::resource('posts', 'PostsController');
 
